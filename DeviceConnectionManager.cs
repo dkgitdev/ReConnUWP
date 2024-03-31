0d028aa180203d2bb9f06f156e022184283f94e7
dk <dk@DESKTOP-BL0F3FN>
2024-03-31 17:41:33 +0300

    Добавьте файлы проекта.

---
+++ DeviceConnectionManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+
+namespace ReConnUWP
+{
+
+	public class DeviceConnectionManager
+	{
+		DeviceInformationCollection devices;
+
+		public class DeviceStatuses
+		{
+			public static readonly string PAIRED = "paired";
+			public static readonly string CONNECTED = "connected";
+			public static readonly string DISCONNECTED = "disconnected";
+			public static readonly string NEW = "new";  // and unpaired as well
+		}
+		public DeviceConnectionManager(){}
+
+		public async void ConnectByName(string name)
+        {
+			await DiscoverPairedDevices();
+            DeviceInformation target = devices.Where((DeviceInformation device) => device.Name == name).First();
+            await RePairDevice(target);
+		}
+
+		public async Task<string> DiscoverPairedDevices()
+		{
+            // TODO: List Devices
+            DeviceInformationCollection deviceInformationCollection = await DeviceInformation.FindAllAsync(BluetoothDevice.GetDeviceSelector());
+			this.devices = deviceInformationCollection;
+			return "Scan complete";
+		}
+
+
+		async Task<string> RePairDevice(DeviceInformation deviceInformation)
+		{
+			// TODO: Connect to Specific device
+			// Should return some interruptable thing
+			try
+			{
+				await deviceInformation.Pairing.UnpairAsync();
+/*				DeviceInformationCollection unpaired = await DeviceInformation.FindAllAsync(BluetoothDevice.GetDeviceSelectorFromPairingState(false));
+				DeviceInformation deviceToPair = unpaired.Where((newDevice) => newDevice.Id == deviceInformation.Id).First();*/
+				await deviceInformation.Pairing.PairAsync();
+				return DeviceStatuses.PAIRED;
+			}
+			catch (Exception e)
+			{
+				return $"Error: {e.Message}";
+			}
+		}
+
+		/*		public async InterruptConnection()
+				{
+					// Try to interrupt connection before it succeeds or fails
+					// Shoud disconnect if it has already successfully connected, using DisconnectDevice
+				}
+
+				public async DisconnectDevice()
+				{
+					// Try to disconnect device
+				}
+			}*/
+
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace ReConnUWP
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly DeviceConnectionManager dm;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            dm = new DeviceConnectionManager();
+        }
+
+        private void BtnScan_Click(object sender, RoutedEventArgs e)
+        {
+            _ = dm.DiscoverPairedDevices();
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            dm.ConnectByName("Joy-Con (R)");
+        }
+    }
+}
